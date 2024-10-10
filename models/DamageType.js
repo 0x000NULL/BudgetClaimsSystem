1cb29b5f63e89ee53e9ba590c7046b9071b32600
0x000NULL <0x000NULL@users.noreply.github.com>
2024-10-10 21:40:03 +0000

    deploy: 27af224eadbe46c9bf275d400cbed4ab2bc55f8e

--- models/DamageType.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DamageTypeSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-});
-
-const DamageType = mongoose.model('DamageType', DamageTypeSchema);
-module.exports = DamageType;
