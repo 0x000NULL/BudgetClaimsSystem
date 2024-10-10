d333206886b7210e8fc347d28a94efd18309c6fe
0x000NULL <0x000NULL@users.noreply.github.com>
2024-10-10 21:40:03 +0000

    deploy: 5755e64b3289fb8fcca743e7e91d33d85d2a2101

--- models/Location.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const LocationSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-});
-
-const Location = mongoose.model('Location', LocationSchema);
-module.exports = Location;
--- models/Status.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const StatusSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-});
-
-const Status = mongoose.model('Status', StatusSchema);
-module.exports = Status;
