fb8cca81c89bfa2d12b749f77f092e35cf7e5c12
Ethan Aldrich <E.aldrich@budgetlasvegas.com>
2024-12-24 07:45:01 -0800

    Added more tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,18 @@ module.exports = {
   verbose: true,
   clearMocks: true,
   testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverage: false,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    'setupServer.js',
+    'routes/**/*.js',
+    'models/**/*.js',
+    'middleware/**/*.js',
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+    '!**/__fixtures__/**'
+  ],
   forceExit: true,
   testTimeout: 10000
 };
